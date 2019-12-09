47e0ba0f9e93d23deabeeca649795ef6092a1bb1
estewart08 <estewart08@gmail.com>
2019-12-09 10:27:07 -0600

    Lulesh verification (#4)
    * Verification enhancement
    
    * Removed extra print statments, added comments for clarification

--- lulesh-mp4/lulesh-util.cc
+++ lulesh-mp4/lulesh-util.cc
@@ -177,7 +177,8 @@ void ParseCommandLineOptions(int argc, char *argv[],
 void VerifyAndWriteFinalOutput(Real_t elapsed_time,
                                Domain& locDom,
                                Int_t nx,
-                               Int_t numRanks)
+                               Int_t numRanks,
+                               Int_t* errors)
 {
    // GrindTime1 only takes a single domain into account, and is thus a good way to measure
    // processor speed indepdendent of MPI parallelism.
@@ -220,5 +221,20 @@ void VerifyAndWriteFinalOutput(Real_t elapsed_time,
    printf("Grind time (us/z/c)  = %10.8g (per dom)  (%10.8g overall)\n", grindTime1, grindTime2);
    printf("FOM                  = %10.8g (z/s)\n\n", 1000.0/grindTime2); // zones per second
 
+   // Validate Results, results should be accurate up to six digits. The e notation will round the sixth digit hence why we are looking for a number between 202507.45 and 202507.54 (inclusive). This final origin energy correct value was taken from the LLNL documents.
+   Real_t finalOriginEnergy = locDom.e(ElemId);
+   Real_t energyMinVal = 202507.45;
+   Real_t energyMaxVal = 202507.54;
+
+   if(finalOriginEnergy < energyMinVal || finalOriginEnergy > energyMaxVal){
+     *errors = 1;
+     printf("***Validation Error: Final Origin Energy incorrect.***\n");
+   }
+   //Any difference larger than 10^-8 is outside margin of error
+   Real_t maxDiff = 0.00000009;
+   if(MaxAbsDiff > maxDiff || TotalAbsDiff > maxDiff || MaxRelDiff > maxDiff){
+     *errors = 1;
+     printf("***Validation Error: Measures of symmetry outside margin of error.***\n");
+   }
    return ;
 }
