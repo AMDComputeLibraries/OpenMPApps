4e0e9ab8aea6501fe21601f6543928a747c2044a
Ron Lieberman <ron.lieberman@amd.com>
2020-04-23 16:30:56 -0500

    Remove old Nekbone

--- Nekbone/test/nek_gpu1/scripts/compare_mpi_master.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-./makenek.mpi  clean
-rm -f compile.mpi.output compile.mpi.error run.mpi.output run.mpi.error
-./makenek.mpi  > compile.mpi.output  2> compile.mpi.error  || cat compile.mpi.error
-mpiexec -n 1 ./nekbone data  > run.mpi.output  2> run.mpi.error  || cat run.mpi.error
-
- vimdiff scripts/master_branch.output run.mpi.output 
-
